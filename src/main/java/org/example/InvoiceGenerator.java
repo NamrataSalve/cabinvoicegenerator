d894233283217773a89b332fa92b9557d0ab909a
NamrataSalve <namratasalve1999@gmail.com>
2024-10-16 16:17:17 +0530

    [Namrata] Added: UC for invoice generator return the enhanced invoice

--- src/main/java/org/example/InvoiceGenerator.java
+++ src/main/java/org/example/InvoiceGenerator.java
@@ -5,9 +5,8 @@ public class InvoiceGenerator {
     public static final double costPerKilometer=10;
     public static final double minFare=5;
 
-    //function for calculate total fare
-
     public double calculateFare(double distance, int time) {
+
         double totalFare=(distance*costPerKilometer)+(time*costTime);
         if (totalFare<minFare){
             return minFare;
@@ -15,24 +14,13 @@ public class InvoiceGenerator {
         return totalFare;
     }
 
-    // Inner class to represent a single ride
-    public static class Ride {
-        double distance;
-        int time;
-
-        public Ride(double distance, int time) {
-            this.distance = distance;
-            this.time = time;
+    public InvoiceSummary calculateFare(Rides[] rides) {
+        double totalFare=0;
+        for (Rides ride : rides)
+        {
+            totalFare+=this.calculateFare(ride.distance, ride.time);
         }
-    }
-
-    // Method to calculate total fare for multiple rides
-    public double calculateFareForMultipleRides(Ride[] rides) {
-        double totalFare = 0;
-        for (Ride ride : rides) {
-            totalFare += calculateFare(ride.distance, ride.time);
-        }
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
     }
 
 }
